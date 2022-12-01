51686a3e1e3c8d2e2cf717a1b831111d8e46d1f7
saito <ibuki1519@outlook.jp>
2022-12-01 11:36:59 +0900

    add a test

--- test/test.bash
+++ test/test.bash
@@ -4,9 +4,9 @@ dir=~
 ["$1" != ""] && dir="$1"
 
 cd $dir/ros2_ws
-colocn build
+colcon build
 source $dir/.bashrc
 timeout 10 ros2 launch mypkg talk_listen_launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log
-gerp 'Listen: 10'
+grep 'Listen: 10'
