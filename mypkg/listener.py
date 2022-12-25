5aceefa42a9fdc28cf6355a1fdf00d40bce8e8fe
ibukisaito <ibuki1519@outlook.jp>
2022-12-26 01:40:51 +0900

    listener.py を更新

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,3 +1,5 @@
+# SPDX-FileCopyrightText: 2022 Ibuki Saito <s21c1050gb@s.chibakoudai.jp>
+# SPDX-License-Identifier: BSD-3-Clause
 import rclpy                #ros2のクライアントのためのライブラリ
 from rclpy.node import Node     #ノードを実装するためのNodeクラス
 from std_msgs.msg import Int16  #通信の型(16ビットの符号付き整数)
